7ccfdaa0b5b36352cd1c9de6180a382256ebb23d
OkuboAtsushi <a2c009bo@gmail.com>
2021-07-10 17:50:54 +0900

    Add urlpatterns of list

--- board_app/urls.py
+++ board_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import signup
+from .views import signup, listfunc
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
+    path('list/', listfunc, name='list'),
 ]
