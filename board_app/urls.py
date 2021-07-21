de2f0b657189ef619fc963b0da7ec5684cfeb412
OkuboAtsushi <a2c009bo@gmail.com>
2021-07-21 21:39:31 +0900

    Add urlpattern of logout

--- board_app/urls.py
+++ board_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import signup, loginfunc, listfunc
+from .views import signup, loginfunc, listfunc, logoutfunc
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', loginfunc, name='login'),
     path('list/', listfunc, name='list'),
+    path('logout/', logoutfunc, name='logout'),
 ]
