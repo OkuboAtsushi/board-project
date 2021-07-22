b72ab4fedcf513b623c7c8c80693f0f22d600856
OkuboAtsushi <a2c009bo@gmail.com>
2021-07-22 11:29:27 +0900

    Fix to allow use of staticfile

--- board_project/settings.py
+++ board_project/settings.py
@@ -115,6 +115,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [str(BASE_DIR / 'board_app/static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
